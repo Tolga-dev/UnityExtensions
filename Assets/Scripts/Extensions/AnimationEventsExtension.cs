8b1cf9a9fb7b85a4f7251d9ca2365fa501796819
Tolga Konat <orhan.konat.12@gmail.com>
2024-12-05 19:43:48 +0300

    Animation Event

---
+++ Assets/Scripts/Extensions/AnimationEventsExtension.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Extensions
+{
+   public static class AnimationEventsExtension {
+
+      public static void AddAnimationEvent(this AnimationClip clip, float time, string functionName) {
+         var clipDuration = clip.length;
+
+         if (time < 0f) {
+            Debug.LogError($"Event time must be greater than or equal to 0.0 seconds.");
+            return;
+         }
+
+         if (time > clipDuration) {
+            Debug.LogError($"Event time must be less than or equal to the clip's duration: {clipDuration} seconds.");
+            return;
+         }
+
+         AnimationEvent animationEvent = new AnimationEvent {
+            time = time,
+            functionName = functionName
+         };
+
+         clip.AddEvent(animationEvent);
+      }
+   }
+
+}
---
+++ Assets/Scripts/Tester.cs
@@ -0,0 +1,26 @@
+﻿using Extensions;
+using UnityEngine ;
+
+
+public class Tester : MonoBehaviour {
+
+   [SerializeField] private AnimationClip clip;
+
+   private void Start() {
+      clip.AddAnimationEvent(1.3f, "OnEventTriggered");
+      clip.AddAnimationEvent(1.5f, "OnFirstEvent");
+      clip.AddAnimationEvent(3.0f, "OnSecondEvent");
+   }
+
+   private void OnEventTriggered() {
+      Debug.Log("Event triggered!");
+   }
+
+   private void OnFirstEvent() {
+      Debug.Log("First event fired!");
+   }
+
+   private void OnSecondEvent() {
+      Debug.Log("Second event fired!");
+   }
+}
